82233bfdcd59fc0037044f44f172d64350d8a556
Brandon Lee <brandonlee2543@hotmail.com>
2024-02-12 16:12:26 -0400

    Created basic Button component with hover detection

---
+++ src/app/CustomButton/CustomButton.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+interface CustomButtonProps {
+  children: string;
+  handleClick: () => void;
+}
+
+const CustomButton = ({ children, handleClick }: CustomButtonProps) => {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+
+  useEffect(() => {
+    console.log({ isHovered });
+  }, [isHovered]);
+  return (
+    <button
+      type="button"
+      className="rounded-[32px] border border-buttonIdle py-4 px-8 font-semibold text-lg text-white"
+      onClick={handleClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <span className="relative">
+        <span>{children}</span>
+        <span className="absolute top-0 left-0 opacity-0">{children}</span>
+      </span>
+    </button>
+  );
+};
+
+export default CustomButton;
